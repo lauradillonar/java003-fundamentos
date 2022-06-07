103079ab65b2167c69f1ebe0fa7224d5880f1333
lauradillonar <laura.dillon.ar@gmail.com>
2022-06-07 16:41:53 -0300

    Uso de properties con ejemplo de generación de POJO

--- src/main/java/com/confluenciacreativa/fundamentos/FundamentosApplication.java
+++ src/main/java/com/confluenciacreativa/fundamentos/FundamentosApplication.java
@@ -1,40 +1,43 @@
 package com.confluenciacreativa.fundamentos;
-
 import com.confluenciacreativa.fundamentos.bean.MyBean;
 import com.confluenciacreativa.fundamentos.bean.MyBeanWithDependency;
 import com.confluenciacreativa.fundamentos.bean.MyBeanWithProperties;
 import com.confluenciacreativa.fundamentos.component.ComponentDependency;
+import com.confluenciacreativa.fundamentos.pojo.UserPojo;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
 public class FundamentosApplication implements CommandLineRunner {
 	private ComponentDependency componentDependency;
 	private MyBean myBean;
 	private MyBeanWithDependency myBeanWithDependency;
 	private MyBeanWithProperties myBeanWithProperties;
+	private UserPojo userPojo;
+
 	public FundamentosApplication(@Qualifier("componentTwoImplement") ComponentDependency componentDependency,
 								  MyBean myBean,
 								  MyBeanWithDependency myBeanWithDependency,
-								  MyBeanWithProperties myBeanWithProperties){
+								  MyBeanWithProperties myBeanWithProperties,
+								  UserPojo userPojo){
 
 		this.componentDependency = componentDependency;
 		this.myBean = myBean;
 		this.myBeanWithDependency = myBeanWithDependency;
 		this.myBeanWithProperties = myBeanWithProperties;
+		this.userPojo = userPojo;
 	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(FundamentosApplication.class, args);
 	}
-
 	@Override
 	public void run(String... args) {
 		componentDependency.saludar();
 		myBean.print();
 		myBeanWithDependency.printWithDependency();
 		System.out.println(myBeanWithProperties.function());
+		System.out.println(userPojo.getEmail() + "-" + userPojo.getPassword());
 	}
 }
--- src/main/java/com/confluenciacreativa/fundamentos/configuration/GeneralConfiguration.java
+++ src/main/java/com/confluenciacreativa/fundamentos/configuration/GeneralConfiguration.java
@@ -2,11 +2,14 @@ package com.confluenciacreativa.fundamentos.configuration;
 
 import com.confluenciacreativa.fundamentos.bean.MyBeanWithProperties;
 import com.confluenciacreativa.fundamentos.bean.MyBeanWithPropertiesImplement;
+import com.confluenciacreativa.fundamentos.pojo.UserPojo;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@EnableConfigurationProperties(UserPojo.class)
 public class GeneralConfiguration {
     @Value("${value.name}")
     private String name;
