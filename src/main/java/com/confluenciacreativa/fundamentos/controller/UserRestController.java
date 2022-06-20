067aeac250e749453d60ca9163a790d0d7c8b546
lauradillonar <laura.dillon.ar@gmail.com>
2022-06-20 12:44:16 -0300

    Pagination con Spring Boot

--- src/main/java/com/confluenciacreativa/fundamentos/controller/UserRestController.java
+++ src/main/java/com/confluenciacreativa/fundamentos/controller/UserRestController.java
@@ -5,6 +5,8 @@ import com.confluenciacreativa.fundamentos.caseuse.DeleteUser;
 import com.confluenciacreativa.fundamentos.caseuse.GetUser;
 import com.confluenciacreativa.fundamentos.caseuse.UpdateUser;
 import com.confluenciacreativa.fundamentos.entity.User;
+import com.confluenciacreativa.fundamentos.repository.UserRepository;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -20,15 +22,18 @@ public class UserRestController {
     private CreateUser createUser;
     private DeleteUser deleteUser;
     private UpdateUser updateUser;
+    private UserRepository userRepository;
 
     public UserRestController(GetUser getUser,
                               CreateUser createUser,
                               DeleteUser deleteUser,
-                              UpdateUser updateUser) {
+                              UpdateUser updateUser,
+                              UserRepository userRepository) {
         this.getUser = getUser;
         this.createUser = createUser;
         this.deleteUser = deleteUser;
         this.updateUser = updateUser;
+        this.userRepository = userRepository;
     }
 
     @GetMapping("/")
@@ -50,5 +55,10 @@ public class UserRestController {
     @PutMapping("/{id}")
     ResponseEntity<User> replaceUser(@RequestBody User newUser, @PathVariable Long id){
         return new ResponseEntity<> (updateUser.update(newUser, id), HttpStatus.OK);
+    }
+
+    @GetMapping("/pageable")
+    List<User> getUserPageable(@RequestParam int page, @RequestParam int size){
+        return userRepository.findAll(PageRequest.of(page,size)).getContent();
     }
 }
--- src/main/java/com/confluenciacreativa/fundamentos/repository/UserRepository.java
+++ src/main/java/com/confluenciacreativa/fundamentos/repository/UserRepository.java
@@ -5,6 +5,7 @@ import com.confluenciacreativa.fundamentos.entity.User;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
@@ -13,7 +14,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends PagingAndSortingRepository<User, Long> {
     @Query("Select u from User u WHERE u.email=?1")
     Optional<User> findByUserEmail(String email);
 
@@ -40,4 +41,6 @@ public interface UserRepository extends JpaRepository<User, Long> {
             " AND u.email=:parametroEmail ")
     Optional<UserDTO> getAllByBirthDateAndEmail(@Param("parametroFecha") LocalDate date,
                                                 @Param("parametroEmail") String email);
+
+    List<User> findAll();
 }
